11bd922ec7bcb2ee47bfdce6b8e63a5cfc742df7
kyle johnson <kyleflynn.johnson@gmail.com>
2023-09-27 17:50:51 +0900

    Updates

---
+++ precache-manifest.e5fa4ab147b1453c29d4108f715c56d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8749c311548f2763e023125f2260a3",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "55d6b801dd2f93ee0312",
+    "url": "/my-portfolio/static/css/main.10fe0f47.chunk.css"
+  },
+  {
+    "revision": "02d9e3223d39d17c2c86",
+    "url": "/my-portfolio/static/js/2.f64ad53e.chunk.js"
+  },
+  {
+    "revision": "37fdc2d2e258a4a0ff71fb7bf84cf091",
+    "url": "/my-portfolio/static/js/2.f64ad53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d6b801dd2f93ee0312",
+    "url": "/my-portfolio/static/js/main.52c35e4c.chunk.js"
+  },
+  {
+    "revision": "a58a32c8cecbb3a88b41",
+    "url": "/my-portfolio/static/js/runtime-main.2c0bc634.js"
+  },
+  {
+    "revision": "9e14855ef75fd749beabd9ca2c1a7577",
+    "url": "/my-portfolio/static/media/discordIcon.9e14855e.svg"
+  },
+  {
+    "revision": "418b0aa275bed12b084b274929ed2ae6",
+    "url": "/my-portfolio/static/media/header__bg.418b0aa2.png"
+  },
+  {
+    "revision": "3282563b313207a990fd684a9f0599e7",
+    "url": "/my-portfolio/static/media/hero-image.3282563b.png"
+  },
+  {
+    "revision": "b47e51889c63ce65354ecb3e3fff531a",
+    "url": "/my-portfolio/static/media/logo_main1.b47e5188.png"
+  },
+  {
+    "revision": "637736a0e412663659b77a029b5cf03a",
+    "url": "/my-portfolio/static/media/skypeIcon.637736a0.png"
+  }
+]);
